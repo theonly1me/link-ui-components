d62528ab9e0236815f41a73251fde8fb0d92f228
Atchyut Preetham Pulavarthi <pulavarthi.preetham@gmail.com>
2021-08-01 16:09:03 +0530

    Update Button.test.tsx

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -5,7 +5,7 @@ import { Default as Button } from '../stories/Button.stories';
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
+    ReactDOM.render(<Button variant="primary"> Test </Button>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
